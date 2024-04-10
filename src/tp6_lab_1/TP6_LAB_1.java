eedf82acc0dd89042dc54d4e304289eb6ae7c6f8
BlasTobares <163307394+BlasTobares@users.noreply.github.com>
2024-04-10 11:02:57 -0300

    subiendo proyecto

---
+++ src/tp6_lab_1/TP6_LAB_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp6_lab_1;
+
+/**
+ *
+ * @author Blas
+ */
+public class TP6_LAB_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
